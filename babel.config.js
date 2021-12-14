99c528f0e1a67bce3a1abf5c0be5e0e66ecdef06
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2021-12-14 13:10:04 +0100

    Milestone status: - game works! -jest works! Note: the error causing the game not to show in the browser was that some module.exports was in the code. This causes the code to stop and wait for the jest test. Solution: either keep all exports at the bottom of game.js (not possible as some exports are from the game scene, and therefore has to be exported in the game scene) or keep module.exports outcommented when the game has to run.

--- babel.config.js
+++ babel.config.js
@@ -16,32 +16,3 @@ module.exports = {
   ],
   plugins: ["@babel/plugin-proposal-class-properties","@babel/plugin-transform-modules-commonjs"]
 };
-
-/*
-{
-  "env": {
-    "test": {
-      "presets": [
-      [
-        "@babel/preset-env",
-        {
-          "targets": {
-            "edge": "17",
-            "firefox": "60",
-            "chrome": "67",
-            "safari": "11.1"
-          },
-          "useBuiltIns": "usage",
-          "corejs": "3.6.5"
-        },
-        "@babel/preset-react"
-      ]
-    ],
-
-
-      "plugins": ["@babel/plugin-proposal-class-properties","@babel/plugin-transform-modules-commonjs"]
-    }
-  }
-}
-
-*/
